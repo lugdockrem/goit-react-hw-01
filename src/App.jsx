55e0aadd8df35135517b370d3e9ca8b009b71c80
ihor_hozhenko <lugtrauma@gmail.com>
2025-01-11 16:00:00 +0000

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import "./App.css";
 import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
-import "./App.css";
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
